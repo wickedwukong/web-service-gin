779ada856f83eeed50a3856779b2fe30e373f828
Xuemin Guan <xueminguan@xuemins-mbp.lan>
2023-10-15 17:58:15 +0100

    XG: insert data to db

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
+	"net/http"
+	"os"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func connectToDb() (*sql.DB, error) {
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+	// Get a database handle.
+	var db *sql.DB
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	return db, err
+}
+func getAlbums(c *gin.Context) {
+	db, err := connectToDb()
+
+	rows, err := db.Query("SELECT * FROM album")
+
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, err)
+	}
+
+	for rows.Next() {
+		var album album
+		err = rows.Scan(&album.ID, &album.Title, &album.Artist, &album.Price)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+		albums = append(albums, album)
+	}
+
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	db, err := connectToDb()
+	if err != nil {
+		return
+	}
+
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, err)
+	}
+
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", newAlbum.Title, newAlbum.Artist, newAlbum.Price)
+	id, err := result.LastInsertId()
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, err)
+	}
+	c.IndentedJSON(http.StatusCreated, gin.H{"id": id})
+
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.Run("localhost:8080")
+}
